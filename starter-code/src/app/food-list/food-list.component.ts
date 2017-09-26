1d82037e0e395760ad9e2ef9f2a5efe04b1db3e2
philipwisner <philipwisner@gmail.com>
2017-09-26 15:23:11 +0200

    iteration 1 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,11 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foods: Object[];
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
 }
