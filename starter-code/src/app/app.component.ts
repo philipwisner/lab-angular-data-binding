02ae417762d0e2898eb4cdefe87e9e9f450fbd36
philipwisner <philipwisner@gmail.com>
2017-09-26 16:20:10 +0200

    added style and hid form

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Food App';
+  show: boolean = false;
+  showFoods: boolean = false;
+
+  showForm(show: boolean) {
+    this.show = true;
+  }
+
+  showFoodsList(showFoods: boolean) {
+    this.showFoods = true;
+  }
 }
